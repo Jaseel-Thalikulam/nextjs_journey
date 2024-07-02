bf2db375004d6e7c0b24482df6a43f308afaa8dd
Jaseel <jaseelta1@gmail.com>
2024-07-02 15:11:11 +0530

    implement route-groups - (auth)

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const signIn = () => {
+  return (
+    <div>signIn</div>
+  )
+}
+
+export default signIn
+
+// Sign-in and sign-up are grouped in the `auth` folder, which acts as a route group. A folder name enclosed in "()" is called a route group.
--- app/_lib/formateDate.ts
+++ app/_lib/formateDate.ts
@@ -1,3 +1,5 @@
 function formateDate() {
     // logical code 
 }
+
+// We can't use `page.tsx` to render any page in private folders. Next.js ignores folders prefixed with "_" for app routing.
