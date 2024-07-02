a67d874b3afeab58288726d183174adb3ca2f039
Jaseel <jaseelta1@gmail.com>
2024-07-02 16:18:19 +0530

    implement title metaData

--- app/products/[productId]/page.tsx
+++ app/products/[productId]/page.tsx
@@ -9,6 +9,17 @@ import React from 'react'
  - we cant export the metaData object and generateMetadata from the same route segment
 */
 
+/**
+ * # The Title Metadata
+ * - Title metadata acts as a string and as an object.
+ * - The object has 3 properties: `absolute`, `template`, and `default`.
+ * - `default` - A default title which will display if there is no other title given in its child page or layout.
+ * - `template` - Acts as a template "%s | some string". The "%s" will be replaced with the title of its child.
+ *      - Example shown in RootLayout and in this page.
+ * - `absolute` - Replaces the complete title without following its parent metadata title template.
+ */
+
+
 type Props = {
   params: {
     productId:string
