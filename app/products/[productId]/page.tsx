a55d35585baaee8fe55a174ab41f548b1d0a21e7
Jaseel <jaseelta1@gmail.com>
2024-07-02 15:56:45 +0530

    implement static and dynamic metadata - products

--- app/products/[productId]/page.tsx
+++ app/products/[productId]/page.tsx
@@ -1,6 +1,27 @@
+import { Metadata } from 'next'
 import React from 'react'
 
-const productDetail = ({params}:{params:{productId:string}}) => {
+/*
+# Implementing dynamic metaData 
+ - with generateMetadata function
+ - the function should be named as "generateMetadata"
+ - we can also fetch the title/description by changing the function to async
+ - we cant export the metaData object and generateMetadata from the same route segment
+*/
+
+type Props = {
+  params: {
+    productId:string
+  }
+}
+
+export const generateMetadata = ({ params }: Props):Metadata => {
+  return {
+    title:`Product ${params.productId}`
+  }
+}
+
+const productDetail = ({params}:Props) => {
   return (
     
       <div>productDetail of product {params.productId}</div>
--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,5 +1,9 @@
+import { Metadata } from 'next';
 import React from 'react'
-
+export const metadata: Metadata = {
+    title: "Products", // This title will replaces the title of root meta data
+    // As the description is not given here so the description will be the default root description.
+  };
 const Products = () => {
   return (
     <div>Products</div>
